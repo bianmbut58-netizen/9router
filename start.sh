fff38322377455c8eed136407444f6cf0e10de94
bianmbut58-netizen <bianmbut58@gmail.com>
2026-07-02 11:56:46 +0700

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,16 +1,4 @@
 #!/bin/bash
 set -e
 
-apt-get update -qq && apt-get install -y -qq curl git unzip
-
-# Hapus dulu kalau sudah ada
-rm -rf /app/9router
-git clone https://github.com/decolua/9router.git /app/9router
-cd /app/9router
-
-cp .env.example .env
-
-npm install
-npm run build
-
-HOSTNAME=0.0.0.0 PORT=${PORT:-8080} npm run start
+exec 9router --no-browser --skip-update
