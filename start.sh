c329c7abdbeb9172cd06ba8a77f8db7f7fe48fe8
bianmbut58-netizen <bianmbut58@gmail.com>
2026-07-02 11:01:03 +0700

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,10 +1,2 @@
 #!/bin/bash
-set -e
-
-# Menyediakan port fallback 20128 jika variabel $PORT dari Railway kosong
-export PORT="${PORT:-20128}"
-
-echo "Memulai 9router pada port: $PORT"
-
-# Menjalankan perintah global 9router
 9router
